8bbe1f163c870705913fad33668e26bcf40747c5
Lucas Napomucena <lucas.nepomucena.ext@spms.min-saude.pt>
2024-09-12 16:07:53 +0100

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import './index.css';
 import 'ol/ol.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './theme';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
       <App />
+      <Analytics />
     </ChakraProvider>
   </StrictMode>,
 );
