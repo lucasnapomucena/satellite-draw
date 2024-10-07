647cddb2d0095aa2ef0a49526c3f2d6eb99d95bb
Lucas Napomucena <lucasnapomucena@gmail.com>
2024-10-07 19:56:26 +0100

    feat: add style to home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,21 @@
 import { ModalForm, MapControls } from '@/modules/home/index';
 import MapComponent from '@/components/Map';
+import { Main } from '@/layout/Main';
+import { useState } from 'react';
+import { S } from './style';
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <MapComponent>
-      <MapControls />
-      <ModalForm />
-    </MapComponent>
+    <Main>
+      <S.MapContent>
+        <MapComponent>
+          <MapControls handleOpenModal={() => setIsOpen(true)} />
+          <ModalForm isOpen={isOpen} onClose={() => setIsOpen(false)} />
+        </MapComponent>
+      </S.MapContent>
+    </Main>
   );
 };
 
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const MapContent = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+export const S = {
+  MapContent,
+};
